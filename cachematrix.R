d7ea3fe3e06c31a9dbc97ed2bae503f92e92e68c
SShaw103 <s.shaw103@gmail.com>
2014-04-27 14:13:48 -0700

    added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,10 @@
+## This set of functions takes a square matrix (that is invertible) 
+##and computes and caches its inverse. Future runs will return
+## the cached value, rather than recomputing it.
+## A good test is a <- makeCacheMatrix(matrix(1:4,2)), which should
+## return       [,1] [,2]
+##        [1,]   -2  1.5
+##        [2,]    1 -0.5
 
 ## makeCacheMatrix creates a list of four functions that will 
 ##operate on a vector x, which is expressed as a square matrix.
